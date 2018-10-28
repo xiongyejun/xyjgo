ef6c1198446e482cdea130aaf6a8e6bec2c941c1
xiongyejun <244114746@qq.com>
2018-10-28 16:33:00 +0800

    add vba dir流的结构

---
+++ vbaDir/vbaDir.go
@@ -0,0 +1,193 @@
+// 解压dir流，获取信息
+package vbaDir
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+
+	"github.com/xiongyejun/xyjgo/ucs2T0utf8"
+)
+
+type dirInfo struct {
+	Name       string
+	TextOffset int32 // stream中开始的位置
+	ModuleType int16
+}
+
+type projectModules struct {
+	Id             int16 // 必须是0x000f
+	Size           int32 // 必须是 0x00000002
+	Count          int16
+	Project_Cookie projectCookie // 8 bytes
+	//Modules
+}
+
+type projectCookie struct {
+	Id     int16 // 必须是0x0013
+	Size   int32 // 必须是 0x00000002
+	Cookie int16 // MUST be ignored on read. MUST be 0xFFFF on write
+}
+type moduleName struct {
+	Id               int16 // 必须是0x0019
+	SizeOfModuleName int32
+	// Dim ModuleName() As Byte
+}
+type moduleNameUnicode struct {
+	Id                      int16 // 必须是0x0047
+	SizeOfModuleNameUnicode int32
+	// Dim ModuleNameUnicode() As Byte
+}
+type moduleStreamName struct {
+	Id               int16 // 必须是0x001A
+	SizeOfStreamName int32
+	// Dim StreamName() As Byte
+}
+type moduleStreamNameUnicode struct {
+	Reserved                int16
+	SizeOfStreamNameUnicode int32
+	// Dim StreamNameUnicode() As Byte
+}
+type moduleDocString struct {
+	Id              int16 // 必须是0x001C
+	SizeOfDocString int32
+	// DocString() As Byte
+}
+
+type moduleDocStringUnicode struct {
+	Reserved               int16
+	SizeOfDocStringUnicode int32
+	// Dim DocStringUnicode() As Byte
+}
+type moduleOffset struct {
+	Id         int16 // 必须是0x0031
+	Size       int32
+	TextOffset int32
+}
+type moduleHelpContext struct {
+	Id          int16 // 必须是0x001E
+	Size        int32
+	HelpContext int32
+}
+type moduleCookie struct {
+	Id     int16 // 必须是0x002C
+	Size   int32 // 必须是 0x00000002
+	Cookie int16 // MUST be 0xFFFF on write
+}
+type moduleType struct {
+	Id int16 //       '0x0021  procedural module
+	// '0x0022 document module, class module, or designer module
+	Reserved int32 //'必须是 0x00000000。必须忽略
+}
+
+func GetModuleInfo(dirStream []byte) (arrDirInfo []dirInfo, err error) {
+	project_Modules := projectModules{}
+
+	var dirStreamLen int32 = int32(len(dirStream))
+	var pDirStream int32 = 0
+	// 找到Project_Modules开始的地址
+	for project_Modules.Id != 0xf ||
+		project_Modules.Size != 2 ||
+		project_Modules.Project_Cookie.Id != 0x13 ||
+		project_Modules.Project_Cookie.Size != 2 {
+
+		pDirStream++
+		if pDirStream > dirStreamLen {
+			return nil, errors.New("不符合dir格式。")
+		}
+		byte2struct(dirStream[pDirStream:], &project_Modules)
+	}
+
+	pDirStream += int32(binary.Size(project_Modules))
+	// 读取模块个数
+	arrDirInfo = make([]dirInfo, project_Modules.Count)
+
+	var i int16 = 0
+	var module_Name moduleName
+	for ; i < project_Modules.Count; i++ {
+		// 读取模块名称
+		module_Name = moduleName{}
+
+		// 找到Module_Name ID =0x0019的地方，
+		//		buf := bytes.NewBuffer(dirStream[pDirStream:])
+		//		binary.Read(buf, binary.LittleEndian, &module_Name)
+
+		byte2struct(dirStream[pDirStream:], &module_Name)
+		//因为有2个结构不一定有，所有不能保证第2后的moduleName位置
+		for module_Name.Id != 0x0019 {
+			pDirStream++
+			byte2struct(dirStream[pDirStream:], &module_Name)
+		}
+		pDirStream += int32(binary.Size(module_Name))
+		pDirStream += module_Name.SizeOfModuleName
+
+		// 读取模块Unicode名称
+		module_NameUnicode := moduleNameUnicode{}
+		byte2struct(dirStream[pDirStream:], &module_NameUnicode)
+		pDirStream += int32(binary.Size(module_NameUnicode))
+		bName := dirStream[pDirStream : pDirStream+module_NameUnicode.SizeOfModuleNameUnicode]
+		bName, _ = ucs2T0utf8.UCS2toUTF8(bName)
+		pDirStream += module_NameUnicode.SizeOfModuleNameUnicode
+		arrDirInfo[i].Name = string(bName)
+
+		// 读取模块stream名称
+		module_StreamName := moduleStreamName{}
+		byte2struct(dirStream[pDirStream:], &module_StreamName)
+		pDirStream += int32(binary.Size(module_StreamName))
+		pDirStream += module_StreamName.SizeOfStreamName
+
+		// 读取模块streamUnicode名称
+		module_StreamNameUnicode := moduleStreamNameUnicode{}
+		byte2struct(dirStream[pDirStream:], &module_StreamNameUnicode)
+		pDirStream += int32(binary.Size(module_StreamNameUnicode))
+		pDirStream += module_StreamNameUnicode.SizeOfStreamNameUnicode
+
+		// 读取模块DocString
+		module_DocString := moduleDocString{}
+		byte2struct(dirStream[pDirStream:], &module_DocString)
+		pDirStream += int32(binary.Size(module_DocString))
+		pDirStream += module_DocString.SizeOfDocString
+
+		// 读取模块ModuleDocStringUnicode
+		module_DocStringUnicode := moduleDocStringUnicode{}
+		byte2struct(dirStream[pDirStream:], &module_DocStringUnicode)
+		pDirStream += int32(binary.Size(module_DocStringUnicode))
+		pDirStream += module_DocStringUnicode.SizeOfDocStringUnicode
+
+		// 读取ModuleOffset
+		module_Offset := moduleOffset{}
+		byte2struct(dirStream[pDirStream:], &module_Offset)
+		pDirStream += int32(binary.Size(module_Offset))
+		arrDirInfo[i].TextOffset = module_Offset.TextOffset
+
+		// 跳过ModuleHelpContext
+		module_HelpContext := moduleHelpContext{}
+		pDirStream += int32(binary.Size(module_HelpContext))
+
+		// 跳过ModuleCookie
+		module_Cookie := moduleCookie{}
+		pDirStream += int32(binary.Size(module_Cookie))
+
+		// 读取Moduletypoe
+		module_type := moduleType{}
+		byte2struct(dirStream[pDirStream:], &module_type)
+		pDirStream += int32(binary.Size(module_type))
+		arrDirInfo[i].ModuleType = module_type.Id
+
+		//            '这2个不一定有！
+		//            ''跳过ModuleReadonly
+		//            'Dim Module_Readonly As ModuleReadonly = Nothing
+		//            'i_start += Marshal.SizeOf(Module_Readonly)
+		//            ''跳过ModulePrivate
+		//            'Dim Module_Private As ModulePrivate = Nothing
+		//            'i_start += Marshal.SizeOf(Module_Private)
+
+	}
+
+	return
+}
+
+func byte2struct(b []byte, pStruct interface{}) {
+	buf := bytes.NewBuffer(b)
+	binary.Read(buf, binary.LittleEndian, pStruct)
+}
