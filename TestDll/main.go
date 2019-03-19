cf64700c0a5c316d7225239519553ac3775683d9
xiongyejun <244114746@qq.com>
2019-03-19 20:05:21 +0800

    TestDll 另一种方法

--- TestDll/main.go
+++ TestDll/main.go
@@ -23,3 +23,13 @@ func Sum(a, b int) int {
 
 // 原文：https://studygolang.com/articles/19000#reply0
 // 编译命令： go build --buildmode=c-shared -o go.dll main.go
+
+// 另一种方法 https://blog.csdn.net/qq_30549833/article/details/86157744
+// 1、go build -v -x -buildmode=c-archive -o xx.a
+//		生成xx.a和xx.h2个文件
+// 2、编写yy.def文件
+//EXPORTS
+//	Sum
+//	Hello
+
+// 3、gcc yy.def xx.a -shared -lwinmm -lWs2_32 -o zz.dll -Wl,--out-implib,zz.lib
