97af6da7236aede585e57140ce970bcd697c6f5d
xiongyejun <244114746@qq.com>
2019-06-23 19:18:23 +0800

    war3

--- war3/war3.go
+++ war3/war3.go
@@ -14,19 +14,23 @@ var PressCount int = 0
 
 func main() {
 	if len(os.Args) != 4 {
-		fmt.Println("war3wcd <key> <mouseClick[0][1]> <PressCount>")
+		fmt.Println("war3 <handle key> <PressCount> <[Mouse(Left=0,Right=1,其他不按)]>")
 		return
 	}
 
 	var err error
-	if PressCount, err = strconv.Atoi(os.Args[3]); err != nil {
+	if PressCount, err = strconv.Atoi(os.Args[2]); err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	key = os.Args[1][0:]
-	if os.Args[2] == "1" {
-		go handleMouse()
+	fmt.Println(key)
+
+	if os.Args[3] == "1" {
+		go handleMouse("right")
+	} else if os.Args[3] == "0" {
+		go handleMouse("left")
 	} else {
 		go handleNoMouse()
 	}
@@ -38,22 +42,22 @@ func main() {
 func handleNoMouse() {
 	for {
 		keve := robotgo.AddEvent(key)
-		if keve == 0 {
+		if keve {
 			for i := 0; i < PressCount; i++ {
 				robotgo.KeyTap(key)
-				robotgo.KeyTap("3")
+				time.Sleep(time.Second / 10)
+				fmt.Println(key)
 			}
 		}
 	}
 }
-func handleMouse() {
+func handleMouse(strMouse string) {
 	for {
 		keve := robotgo.AddEvent(key)
-		if keve == 0 {
+		if keve {
 			for i := 0; i < PressCount; i++ {
-				robotgo.KeyTap(key)
-				robotgo.MouseClick()
-				robotgo.KeyTap("3")
+				robotgo.MouseClick(strMouse)
+				time.Sleep(time.Second / 10)
 			}
 		}
 	}
