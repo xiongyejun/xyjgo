9130c42d47577ac0223379c791565b22371d6f19
xiongyejun <244114746@qq.com>
2019-07-19 20:54:49 +0800

    modified build.cmd

--- TestDll/build_x86.cmd
+++ TestDll/build_x86.cmd
@@ -1,12 +1,12 @@
-REM 编译32位的dll
+﻿REM 编译32位的dll
 
 set GOARCH=386
-set GOROOT=C:\Users\Administrator\Downloads\go386
+set GOROOT=C:\go386
 
 set "str=%path%"
 set path=C:\mingw32\MinGW\bin;%str%
 
-C:\Users\Administrator\Downloads\go386\bin\go.exe build -v -x -buildmode=c-archive -o go.a
+C:\go386\bin\go.exe build -v -x -buildmode=c-archive -o go.a
 gcc.exe go.def go.a -shared -lwinmm -lWs2_32 -o go.dll -Wl,--out-implib,go.lib
 
 set path = %str%
