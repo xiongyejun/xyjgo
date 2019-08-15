6d9caf44c98441ecebae9a525659f76d1084344a
xiongyejun <244114746@qq.com>
2019-08-15 20:04:08 +0800

    reg

--- reg/reg_test.go
+++ reg/reg_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/xiongyejun/xyjgo/ucs2T0utf8"
+	"github.com/xiongyejun/xyjgo/ucs2"
 	"github.com/xiongyejun/xyjgo/winAPI/advapi32"
 )
 
@@ -18,7 +18,7 @@ func Test_func(t *testing.T) {
 
 	for i := range rv {
 		if rv[i].Type == advapi32.REG_SZ {
-			rv[i].Data, err = ucs2T0utf8.UCS2toUTF8(rv[i].Data)
+			rv[i].Data, err = ucs2.ToUTF8(rv[i].Data)
 			if err != nil {
 				t.Log(err)
 				return
@@ -30,9 +30,10 @@ func Test_func(t *testing.T) {
 
 	}
 	//	printhex(`%SystemRoot%\Explorer.exe`)
-	//	printhex(`%GOPATH%\bin\sendMail.exe`)
+	printhex(`%GOPATH%\bin\sendMail.exe`)
 }
 
+// 可以先在注册表中导出1个项目看看
 // 注册表使用环境变量的时候，需要把类型设置为REG_EXPAND_SZ，但是regedit好像不能直接编辑
 // 用.reg文件导入的时候，又需要使用hex16进制数据，用printhex输出，最后的逗号不需要
 func printhex(str string) {
